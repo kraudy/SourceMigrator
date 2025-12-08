7d89b4553382c4b88e1877665c17ca685cb6fdbb
Roberto Torrez <robkraudy@gmail.com>
2025-12-07 20:33:00 -0600

    Remove ' from paths
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- src/main/java/com/github/kraudy/migrator/SourceMigrator.java
+++ src/main/java/com/github/kraudy/migrator/SourceMigrator.java
@@ -436,7 +436,7 @@ public class SourceMigrator implements Runnable{
           System.out.println("Migrated SourcePf: " + sourcePf + " | member: " + memberName + "." + sourceType + ": OK");
           totalMembersMigrated++;
           if (returnPaths){
-            migratedPaths.add("'" + ifsOutputDir + "/" + memberName + "." + sourceType + "'");
+            migratedPaths.add(ifsOutputDir + "/" + memberName + "." + sourceType);
           }
         }
 
